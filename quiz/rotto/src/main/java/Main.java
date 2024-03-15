79c27d644456e36144e61fd6108fa3a488abff94
chaeyeon-yang <myday0827@naver.com>
2024-03-15 18:44:05 +0900

    quiz: 로또 번호 발생기

--- quiz/rotto/src/main/java/Main.java
+++ quiz/rotto/src/main/java/Main.java
@@ -4,18 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
 
-        int rottoNum[] = new int[6];
-        boolean checkNum[] = new boolean[46];
+        boolean checkNum[] = new boolean[47];
 
         int cnt = 0;
-        while (rottoNum.length<6) {
-            // 1부터 46까지의 난수 발생
+        while (cnt<6) {
+            // 1부터 46까지의 å난수 발생
             int randomNum = random.nextInt(46)+1;
             if (!checkNum[randomNum]) {
-                rottoNum[cnt] = randomNum;
+                checkNum[randomNum] = true;
                 cnt++;
+                System.out.printf("%d ",randomNum);
             } else {
-                System.out.println("(중복발생! 재추첨)");
+                System.out.printf("%d(중복발생! 재추첨) ", randomNum);
             }
         }
     }
